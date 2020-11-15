92e61a44a1da7c543855545f17179170d81c0475
rayhanendra <48467097+rayhanendra@users.noreply.github.com>
2020-11-15 21:42:36 +0700

    Update Sekuens Collatz - Shared Memory.cpp

--- Sekuens Collatz - Shared Memory.cpp
+++ Sekuens Collatz - Shared Memory.cpp
@@ -1,57 +1,63 @@
-/*Ini adalah proses mengimplementasikan konjektur Collatz ke shared memory
-John Wiley & Sons, 2013*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/shm.h>
+#include <unistd.h>	/*untuk ftruncate*/
+#include <wait.h>	 /*untuk  wait*/
+#include <sys/mman.h>	 /*untuk  shm_open*/
+#include <fcntl.h>	/*untuk O_* konstan*/
 #include <sys/stat.h>
-#include <sys/mman.h>
-
-int main(int argc, char *argv[])
-{
- const int SIZE = 4096; 		/*ukuran objek shared memory dalam bytes*/
- const char *name = "COLLATZ";	 /*nama dari objek shared memory*/
-
- int shm_fd;				/*pendeskripsi file shared memory*/
- void *ptr;				/*pointer ke objek shared memory*/
- char str[128];
- int n = atoi(argv[1]);
-
-/*buat segmen shared memory */
- shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666); 			
-
-/* konfigurasi ukuran size dari segmen shared memory*/
- ftruncate(shm_fd,SIZE);  							
-
- /* map segmen shared memory di alamat proses */
- ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
- if (ptr == MAP_FAILED) {
-  printf("Map failed\n");
-  exit(-1);
- }
+#include <sys/types.h>
  
- /*Tulis collatz sequence ke bagian shared memory, dan kita perlu melakukan penambahan nilai dari pointer setiap setelah melakukan write */
+int main(int argc, char **argv){
+    /*buat area shared memory.*/
+    const int SIZE = 4096;
+    const char *name ="OS";
+    const int BUFFER_SIZE = 1024;
+    int shm_fd;
+    void *ptr;
+ 
+    shm_fd = shm_open(name,O_CREAT | O_RDWR,0666); 
+    ftruncate(shm_fd,SIZE);	/*potong file*/
+    ptr = mmap(0,SIZE,PROT_WRITE,MAP_SHARED,shm_fd,0);	/* Map file ke memory*/
+    /*buat proses*/
+    pid_t pid;
+    pid = fork();
+    if (pid < 0){
+    	fprintf(stderr,"Fork Failed\n");
+    	shm_unlink(name);
+    	return 1;}
+    else if (pid ==0){
+    	/*membuat proses child*/
+    	char buffer[BUFFER_SIZE];
+    	memset(buffer,0,sizeof(char)*BUFFER_SIZE);
+    	char *buffer_p = &buffer[0];
+    	if (argc == 1 || argc > 2){
+        	fprintf(stderr,"Pass invaild args!\n");
+        	shm_unlink(name);
+        	return 1;}
+     
+     int num = atoi(argv[1]);
+    	buffer_p += sprintf(buffer_p,"%d,",num);
+    	while (num != 1){
+        	if (num % 2 == 0)/*Even*/{
+            	num = num / 2;
+            	if (num == 1){
+                	buffer_p += sprintf(buffer_p,"%d\n",num);}
+            	else{
+                	buffer_p += sprintf(buffer_p,"%d,",num);}	}
+        	else{
+            	num = 3*num + 1;
+            	buffer_p += sprintf(buffer_p,"%d,",num);}}
+    	sprintf(ptr,"%s",buffer);
+    	printf("The data to shared memory has written.\n");}
+    else{
+    	/*Proses Parent*/
+    	wait(NULL);
+    	printf("Reading the shared memory\n");
+    	shm_fd = shm_open(name,O_RDONLY, 0666);
+    	ptr = mmap(0,SIZE,PROT_READ,MAP_SHARED,shm_fd,0);
+    	printf("%s",(char *)ptr);
+    	shm_unlink(name);}
+    return 0;}
 
- sprintf(str,"%d, ", n);
- sprintf(ptr,"%s",str);
- while (n != 1) {
-  ptr += strlen(str);
-
-  if (n % 2 == 0)
-   n = n / 2;
-  else
-   n = 3 * n + 1;
-
-  sprintf(str,"%d, ", n);
-  sprintf(ptr,"%s",str);
- }
-
- return 0;
-}
-
-
-
-
-
+// sumber kode : https://zhuanlan.zhihu.com/p/80883605
